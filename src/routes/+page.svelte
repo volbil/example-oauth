27788695961f248f01c996f1efc6c9485255bffe
volbil <volbilnexus@gmail.com>
2023-07-16 21:02:26 +0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { page } from '$app/stores'
+
+    const url = $page.url
+    
+    const code = url.searchParams.get('code')
+
+    let result = "Result..."
+
+    const testRequest = (code) => {
+        fetch('http://127.0.0.1:8888/auth/test', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "code": code
+            })
+        }).then((response) => response.json()).then((data) => {
+            result = JSON.stringify(data)
+        })
+    }
+</script>
+
+<div>
+    Code: <span>{code}</span>
+    <div style="margin-top: 20px; margin-bottom: 20px;">
+        <button on:click={() => testRequest(code)}>Test test</button>
+    </div>
+    <div>{result}</div>
+</div>
